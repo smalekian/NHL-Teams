46c562aa863c41ae547cdf004b3888f75423304f
Spencer Malekian <spencermalekian@ip-10-200-0-52.ec2.internal>
2018-09-06 09:11:17 -0400

    seeds back to ints, data type in initial migration changed accordingly

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,12 +5,12 @@
 # Inside the script, you can read and write to any of your
 # repositories directly:
 #
-  NhlTeams.Repo.insert!(%NhlTeams.Team{location: "New Jersey", mascot: "Devils", division: "Metropolitan", division_id: "0"})
-  NhlTeams.Repo.insert!(%NhlTeams.Team{location: "New York", mascot: "Rangers", division: "Metropolitan", division_id: "0"})
-  NhlTeams.Repo.insert!(%NhlTeams.Team{location: "Detroit", mascot: "Red Wings", division: "Atlantic", division_id: "1"})
-  NhlTeams.Repo.insert!(%NhlTeams.Team{location: "Boston", mascot: "Bruins", division: "Atlantic", division_id: "1"})
-  NhlTeams.Repo.insert!(%NhlTeams.Team{location: "Nashville", mascot: "Predators", division: "Central", division_id: "2"})  
-  NhlTeams.Repo.insert!(%NhlTeams.Team{location: "Las Vegas", mascot: "Golden Knights", division: "Pacific", division_id: "3"})  
+  NhlTeams.Repo.insert!(%NhlTeams.Team{location: "New Jersey", mascot: "Devils", division: "Metropolitan", division_id: 0})
+  NhlTeams.Repo.insert!(%NhlTeams.Team{location: "New York", mascot: "Rangers", division: "Metropolitan", division_id: 0})
+  NhlTeams.Repo.insert!(%NhlTeams.Team{location: "Detroit", mascot: "Red Wings", division: "Atlantic", division_id: 1})
+  NhlTeams.Repo.insert!(%NhlTeams.Team{location: "Boston", mascot: "Bruins", division: "Atlantic", division_id: 1})
+  NhlTeams.Repo.insert!(%NhlTeams.Team{location: "Nashville", mascot: "Predators", division: "Central", division_id: 2})  
+  NhlTeams.Repo.insert!(%NhlTeams.Team{location: "Las Vegas", mascot: "Golden Knights", division: "Pacific", division_id: 3})  
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
